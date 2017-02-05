8d8bb5f16f7ff3aeb9fb9eb3350984912f825d0c
Joel <pratt.joel@gmail.com>
2017-02-04 23:56:49 -0500

    Add test UI for contac form

---
+++ ChurchBlogger/ContactFormVC.swift
@@ -0,0 +1,21 @@
+//
+//  ContactFormVC.swift
+//  ChurchBlogger
+//
+//  Created by Joel on 2/4/17.
+//  Copyright © 2017 Joel Pratt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ContactFormVC: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    
+    @IBAction func sendContactInfo(_ sender: Any) {
+    }
+}
