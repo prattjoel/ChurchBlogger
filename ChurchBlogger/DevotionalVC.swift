33c180b5d68883d5d60c1218b13ecb99fb5e6185
Joel <pratt.joel@gmail.com>
2017-02-08 22:07:41 -0500

    Setup test UI for DevotionalVC

---
+++ ChurchBlogger/DevotionalVC.swift
@@ -0,0 +1,15 @@
+//
+//  DevotionalVC.swift
+//  ChurchBlogger
+//
+//  Created by Joel on 2/8/17.
+//  Copyright © 2017 Joel Pratt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DevotionalVC: UIViewController {
+    
+    @IBOutlet weak var devotionalText: UITextView!
+}
