922846103635d89e17aca5b2d12266a2dc4b4caf
Joel <pratt.joel@gmail.com>
2017-02-09 14:19:50 -0500

    Add constants for WordPress client url

---
+++ ChurchBlogger/WordPressClient.swift
@@ -0,0 +1,13 @@
+//
+//  WordPressClient.swift
+//  ChurchBlogger
+//
+//  Created by Joel on 2/9/17.
+//  Copyright © 2017 Joel Pratt. All rights reserved.
+//
+
+import Foundation
+
+class WordPressClient {
+    
+}
---
+++ ChurchBlogger/WordPressConstants.swift
@@ -0,0 +1,19 @@
+//
+//  WordPressConstants.swift
+//  ChurchBlogger
+//
+//  Created by Joel on 2/9/17.
+//  Copyright © 2017 Joel Pratt. All rights reserved.
+//
+
+import Foundation
+
+extension WordPressClient {
+    
+    struct Components {
+        static let ApiScheme = "http"
+        static let ApiHost = "www.theteslablog.com"
+        static let ApiPath = "/wp-json/wp/v2/"
+        static let PagesMethod = "pages"
+    }
+}
